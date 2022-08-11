2c5f84b49e64b421523be9eeaa372c53a24c38ff
Tien Dao <tiendao5240@gmail.com>
2022-08-11 10:01:18 -0400

    EggTimer-iOS13 first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    var secondRemaining = 60
+    let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        secondRemaining = eggTimes[hardness]!
 
+        ///Argument "#selector"  refers to instance method "updateTimer()" exposed to Objective-C
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if secondRemaining>0 {
+            print("\(secondRemaining) seconds.")
+            secondRemaining-=1
+        }
+    }
 }
