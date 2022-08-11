92bd1289641956528cd1e13bbe166153a7d75902
Tien Dao <tiendao5240@gmail.com>
2022-08-11 10:37:41 -0400

    Added play alarm

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,26 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     let softTime = 5
     let mediumTime = 7
     let hardTime = 12
-    var secondRemaining = 60
-    let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
+    
+    let eggTimes = ["Soft":3, "Medium":420, "Hard":720]
+    
+    var totalTime = 0
+    var secondsPassed = 0
     
     ///Create new timer
     var timer = Timer()
 
+    ///player for Audio
+//    var player = AVAudioPlayer()
+    
+    ///Updating AVAudioPlayer initialisation
+    var player : AVAudioPlayer!
+    
     @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+
         
         ///Stop the previous timer
         timer.invalidate()
         
         let hardness = sender.currentTitle!
-        secondRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
 
         ///Argument "#selector"  refers to instance method "updateTimer()" exposed to Objective-C
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
@@ -34,12 +52,20 @@ class ViewController: UIViewController {
     }
     
     @objc func updateTimer() {
-        if secondRemaining>0 {
-            print("\(secondRemaining) seconds.")
-            secondRemaining-=1
+        if secondsPassed < totalTime {
+            secondsPassed+=1
+            let percentageProgress = Float(secondsPassed)/Float(totalTime)
+            print(percentageProgress)
+            progressBar.progress = Float(percentageProgress)
+            print(Float(percentageProgress))
+            
         } else {
             timer.invalidate()
             titleLabel.text = "Done"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
 }
