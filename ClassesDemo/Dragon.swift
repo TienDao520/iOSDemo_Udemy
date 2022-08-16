d06fc6e628631798ff608403de9b4dab8d1a1dad
Tien Dao <tiendao5240@gmail.com>
2022-08-16 18:03:14 -0400

    call method of super class inside inheriting class

--- ClassesDemo/Dragon.swift
+++ ClassesDemo/Dragon.swift
@@ -9,4 +9,9 @@ class Dragon: Enemy {
     override func move() {
         print("Fly forwards")
     }
+    
+    override func attack() {
+        super.attack()
+        print("Spits fire, does 10 damage")
+    }
 }
